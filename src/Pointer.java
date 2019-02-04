17824765a0649cd454ac5be8957b77aa5c0f3867
Boshaw <boshaw@protonmail.com>
2019-02-04 05:46:16 -0500

    - Quest pointer icon should be fixed now to not disappear or show wrong direction, i believe. You can also click the     icon to go to it (up to 1000 tiles at a time) or interact with it (right click menu) if in view distance.

--- src/Pointer.java
+++ src/Pointer.java
@@ -1,4 +1,8 @@
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
 import haven.*;
+
+import static haven.OCache.posres;
 
 public class Pointer extends Widget {
     public static final States.ColState col = new States.ColState(241, 227, 157, 255);
@@ -8,16 +12,14 @@ public class Pointer extends Widget {
     public Coord lc;
     public long gobid = -1L;
 
-    public Pointer(Indir<Resource> paramIndir)
-    {
+    public Pointer(Indir<Resource> res) {
 	super(Coord.z);
-	this.icon = paramIndir;
+	this.icon = res;
     }
 
-    public static Widget mkwidget(UI paramUI, Object... paramVarArgs)
-    {
-	int i = ((Integer)paramVarArgs[0]);
-	return new Pointer(i < 0 ? null : paramUI.sess.getres(i));
+    public static Widget mkwidget(UI ui, Object... args) {
+	int i = ((Integer)args[0]);
+	return new Pointer(i < 0 ? null : ui.sess.getres(i));
     }
 
     public void presize()
@@ -25,14 +27,12 @@ public class Pointer extends Widget {
 	resize(this.parent.sz);
     }
 
-    protected void added()
-    {
+    protected void added() {
 	presize();
 	super.added();
     }
 
-    private int signum(int paramInt)
-    {
+    private int signum(int paramInt) {
 	if (paramInt < 0) {
 	    return -1;
 	}
@@ -42,35 +42,34 @@ public class Pointer extends Widget {
 	return 0;
     }
 
-    private void drawarrow(GOut paramGOut, Coord paramCoord)
-    {
-	Coord localCoord1 = this.sz.div(2);
-	paramCoord = paramCoord.sub(localCoord1);
-	if (paramCoord.equals(Coord.z)) {
-	    paramCoord = new Coord(1, 1);
+    private void drawarrow(GOut g, Coord sc) {
+	Coord hsz = this.sz.div(2);
+	sc = sc.sub(hsz);
+	if (sc.equals(Coord.z)) {
+	    sc = new Coord(1, 1);
 	}
-	double d = Coord.z.dist(paramCoord);
-	Coord localCoord2 = paramCoord.mul((d - 25.0D) / d);
-	float f = localCoord1.y / localCoord1.x;
-	if ((Math.abs(localCoord2.x) > localCoord1.x) || (Math.abs(localCoord2.y) > localCoord1.y)) {
+	double d = Coord.z.dist(sc);
+	Coord localCoord2 = sc.mul((d - 25.0D) / d);
+	float f = hsz.y / (float)hsz.x;
+	if ((Math.abs(localCoord2.x) > hsz.x) || (Math.abs(localCoord2.y) > hsz.y)) {
 	    if (Math.abs(localCoord2.x) * f < Math.abs(localCoord2.y)) {
-		localCoord2 = new Coord(localCoord2.x * localCoord1.y / localCoord2.y, localCoord1.y).mul(signum(localCoord2.y));
+		localCoord2 = new Coord(localCoord2.x * hsz.y / localCoord2.y, hsz.y).mul(signum(localCoord2.y));
 	    } else {
-		localCoord2 = new Coord(localCoord1.x, localCoord2.y * localCoord1.x / localCoord2.x).mul(signum(localCoord2.x));
+		localCoord2 = new Coord(hsz.x, localCoord2.y * hsz.x / localCoord2.x).mul(signum(localCoord2.x));
 	    }
 	}
-	Coord localCoord3 = localCoord2.sub(paramCoord).norm(30.0D);
-	localCoord2 = localCoord2.add(localCoord1);
+	Coord localCoord3 = localCoord2.sub(sc).norm(30.0D);
+	localCoord2 = localCoord2.add(hsz);
 
-	BGL localBGL = paramGOut.gl;
-	paramGOut.state2d();
-	paramGOut.state(col);
-	paramGOut.apply();
+	BGL localBGL = g.gl;
+	g.state2d();
+	g.state(col);
+	g.apply();
 	localBGL.glEnable(2881);
 	localBGL.glBegin(4);
-	paramGOut.vertex(localCoord2);
-	paramGOut.vertex(localCoord2.add(localCoord3).add(-localCoord3.y / 3, localCoord3.x / 3));
-	paramGOut.vertex(localCoord2.add(localCoord3).add(localCoord3.y / 3, -localCoord3.x / 3));
+	g.vertex(localCoord2); //The point
+	g.vertex(localCoord2.add(localCoord3).add(-localCoord3.y / 3, localCoord3.x / 3)); //one side
+	g.vertex(localCoord2.add(localCoord3).add(localCoord3.y / 3, -localCoord3.x / 3)); //The other
 	localBGL.glEnd();
 	localBGL.glDisable(2881);
 	if (this.icon != null) {
@@ -79,76 +78,131 @@ public class Pointer extends Widget {
 		if (this.licon == null) {
 		    this.licon = ((this.icon.get()).layer(Resource.imgc)).tex();
 		}
-		paramGOut.aimage(this.licon, localCoord2.add(localCoord3), 0.5D, 0.5D);
+		g.aimage(this.licon, localCoord2.add(localCoord3), 0.5D, 0.5D);
 	    }
 	    catch (Loading localLoading) {}
 	}
 	this.lc = localCoord2.add(localCoord3);
     }
 
-    public void draw(GOut paramGOut)
-    {
+    private void drawarrow(GOut g, double a) {
+	Coord hsz = sz.div(2);
+	double ca = -Coord.z.angle(hsz);
+	Coord ac;
+	if((a > ca) && (a < -ca)) {
+	    ac = new Coord(sz.x, hsz.y - (int)(Math.tan(a) * hsz.x));
+	} else if((a > -ca) && (a < Math.PI + ca)) {
+	    ac = new Coord(hsz.x - (int)(Math.tan(a - Math.PI / 2) * hsz.y), 0);
+	} else if((a > -Math.PI - ca) && (a < ca)) {
+	    ac = new Coord(hsz.x + (int)(Math.tan(a + Math.PI / 2) * hsz.y), sz.y);
+	} else {
+	    ac = new Coord(0, hsz.y + (int)(Math.tan(a) * hsz.x));
+	}
+	Coord bc = ac.add(Coord.sc(a, 0));
+
+	g.state2d();
+	g.state(col);
+	g.apply();
+	g.gl.glEnable(GL2.GL_POLYGON_SMOOTH);
+	g.gl.glBegin(GL.GL_TRIANGLES);
+	g.vertex(bc);
+	g.vertex(bc.add(Coord.sc(a + Math.PI / 12, -35)));
+	g.vertex(bc.add(Coord.sc(a - Math.PI / 12, -35)));
+	g.gl.glEnd();
+	g.gl.glDisable(GL2.GL_POLYGON_SMOOTH);
+
+	if (this.icon != null) {
+	    try {
+		if (this.licon == null) {
+		    this.licon = ((this.icon.get()).layer(Resource.imgc)).tex();
+		}
+		g.aimage(this.licon, bc.add(Coord.sc(a, -30)), 0.5, 0.5);
+	    }
+	    catch (Loading localLoading) {
+	        //Ignore it
+	    }
+	}
+	this.lc = bc.add(Coord.sc(a, -30));
+    }
+
+    public void draw(GOut g) {
 	this.lc = null;
 	if (this.tc == null) {
 	    return;
 	}
-	Gob localGob = this.gobid < 0L ? null : this.ui.sess.glob.oc.getgob(this.gobid);
-	Coord3f localCoord3f;
-	if (localGob != null) {
-	    try
-	    {
-		localCoord3f = (getparent(GameUI.class)).map.screenxf(localGob.getc());
-	    }
-	    catch (Loading localLoading)
-	    {
+
+	Gob questgob = this.gobid < 0L ? null : this.ui.sess.glob.oc.getgob(this.gobid);
+	final Coord3f gobsc;
+	final Coord2d gobrc;
+	if (questgob != null) {
+	    try {
+		gobsc = (getparent(GameUI.class)).map.screenxf(questgob.getc());
+		gobrc = questgob.rc;
+	    } catch (Loading localLoading) {
 		return;
 	    }
 	} else {
-	    localCoord3f = (getparent(GameUI.class)).map.screenxf(this.tc);
+	    gobsc = ui.gui.map.screenxf(this.tc);
+	    gobrc = tc;
 	}
-	if (localCoord3f != null) {
-	    drawarrow(paramGOut, new Coord(localCoord3f));
+
+	if (gobsc != null) {
+	    final Double angle = ui.gui.map.screenangle(gobrc, true);
+	    if(!angle.equals(Double.NaN)) {
+		drawarrow(g, ui.gui.map.screenangle(gobrc, true));
+	    } else {
+		drawarrow(g, new Coord(gobsc));
+	    }
 	}
     }
 
-    public void udpate(Coord2d paramCoord2d, long paramLong)
-    {
+    public void udpate(Coord2d paramCoord2d, long paramLong) {
 	this.tc = paramCoord2d;
 	this.gobid = paramLong;
     }
 
-    public void uimsg(String paramString, Object... paramVarArgs)
-    {
-	if (paramString.equals("upd"))
-	{
-	    if (paramVarArgs[0] == null) {
-		this.tc = null;
-	    } else {
-		this.tc = ((Coord)paramVarArgs[0]).mul(OCache.posres);
-	    }
-	    if (paramVarArgs[1] == null) {
-		this.gobid = -1L;
-	    } else {
-		this.gobid = ((Integer)paramVarArgs[1]);
-	    }
+    public void uimsg(String msg, Object... args) {
+        switch (msg) {
+	    case "upd":
+		if (args[0] == null) {
+		    this.tc = null;
+		} else {
+		    this.tc = ((Coord)args[0]).mul(posres);
+		}
+		if (args[1] == null) {
+		    this.gobid = -1L;
+		} else {
+		    this.gobid = ((Integer)args[1]);
+		}
+	        break;
+	    case "icon":
+		int i = ((Integer)args[0]);
+		this.icon = i < 0 ? null : this.ui.sess.getres(i);
+		this.licon = null;
+	        break;
+	    default:
+		super.uimsg(msg, args);
 	}
-	else if (paramString.equals("icon"))
-	{
-	    int i = ((Integer)paramVarArgs[0]);
-	    this.icon = i < 0 ? null : this.ui.sess.getres(i);
-	    this.licon = null;
-	}
-	else
-	{
-	    super.uimsg(paramString, paramVarArgs);
+    }
+
+    @Override
+    public boolean mousedown(Coord c, int button) {
+        if(this.lc != null && this.lc.dist(c) < 20.0) {
+            if(gobid > 0) {
+		ui.gui.map.wdgmsg("click", rootpos().add(c), this.tc.floor(posres), button, ui.modflags(), 0, (int) gobid, this.tc.floor(posres), 0, -1);
+	    } else {
+                ui.gui.map.wdgmsg("click", rootpos().add(c), this.tc.floor(posres), button, ui.modflags());
+	    }
+            return true;
+	} else {
+            return super.mousedown(c, button);
 	}
     }
 
     private Text.Line tt = null;
     private int dist;
-    public Object tooltip(Coord paramCoord, Widget paramWidget)
-    {
-	if ((this.lc != null) && (this.lc.dist(paramCoord) < 20.0D)) {
+    public Object tooltip(Coord c, Widget wdg) {
+	if ((this.lc != null) && (this.lc.dist(c) < 20.0D)) {
 	    if(tooltip instanceof Text.Line) {
 	        final Gob me = ui.sess.glob.oc.getgob(ui.gui.map.plgob);
 	        final int cdist = (int)(Math.ceil(me.rc.dist(tc)/11.0));
